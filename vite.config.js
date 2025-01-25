3ed4a842975392e45cb88c96076f90c824c2a97a
MiralPatel-017 <miralkamleshbhai@gmail.com>
2025-01-25 12:26:38 +0530

    modified:   package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+    },
+  },
   build: {
-    outDir: 'dist', // Default output directory
+    rollupOptions: {
+      external: ['react-icons/li'],
+    },
   },
 });
